aaedacbf642b4428e0c7556b4be759fe831f607a
Hallvard Traetteberg <hal@ntnu.no>
2020-01-21 10:20:30 +0100

    More features, supports most core tasks.

--- no.hal.timers/fxui/src/main/java/no/hal/timers/fxui/AppController.java
+++ no.hal.timers/fxui/src/main/java/no/hal/timers/fxui/AppController.java
@@ -6,11 +6,8 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
-import java.util.function.Supplier;
 import java.util.stream.Collectors;
 
 import javafx.animation.KeyFrame;
@@ -22,8 +19,12 @@ import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleButton;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.util.Callback;
 import no.hal.timers.core.Competition;
+import no.hal.timers.core.Participant;
 import no.hal.timers.core.Participation;
 import no.hal.timers.core.TimeProvider;
 import no.hal.timers.csv.CompetitionCsvReader;
@@ -48,7 +49,7 @@ public class AppController {
 	private Button startButton;
 
 	@FXML
-	private TextField timingKeyTextField;
+	private Button timeButton;
 
 	@FXML
 	private ToggleButton editTimingKeysButton;
@@ -57,11 +58,17 @@ public class AppController {
 	private Button addTimingKeyButton;
 
 	@FXML
+	private ToggleButton editParticipantNamesButton;
+
+	@FXML
+	private Button addParticipantButton;
+
+	@FXML
 	public void initialize() {
 		if (timeLabel != null) {
-			final Timeline timeline = new Timeline();
+			final var timeline = new Timeline();
 			timeline.setCycleCount(Timeline.INDEFINITE);
-			timeline.getKeyFrames().add(new KeyFrame(javafx.util.Duration.seconds(1), event -> updateTimeLabel()));
+			timeline.getKeyFrames().add(new KeyFrame(javafx.util.Duration.seconds(1), event -> handleTimeChanged()));
 			timeline.playFromStart();
 		}
 		final CompetitionCsvReader reader = new CompetitionCsvReader();
@@ -70,6 +77,7 @@ public class AppController {
 			updateGrid();
 			clearSelection();
 		} catch (final Exception e) {
+			e.printStackTrace(System.err);
 		}
 	}
 
@@ -91,8 +99,14 @@ public class AppController {
 		return (time != null ? formatTime(time.toHoursPart(), time.toMinutesPart(), time.toSecondsPart(), true) : "__:__");
 	}
 
-	private void updateTimeLabel() {
-		timeLabel.setText(formatTime(timeProvider.get()));
+	private final Collection<Callback<LocalTime, Void>> timeCallbacks = new ArrayList<>();
+
+	private void handleTimeChanged() {
+		final LocalTime time = timeProvider.get();
+		timeLabel.setText(formatTime(time));
+		for (final Callback<LocalTime, Void> timeCallback : timeCallbacks) {
+			timeCallback.call(time);
+		}
 	}
 
 	private final Collection<Participation> selection = new ArrayList<>();
@@ -109,13 +123,13 @@ public class AppController {
 	private void setSelection(final Collection<Participation> newSelection) {
 		selection.clear();
 		selection.addAll(newSelection);
-		for (final Participation participation : selectParticipationButtons.keySet()) {
+		for (final var participation : selectParticipationButtons.keySet()) {
 			setSelected(participation, newSelection.contains(participation));
 		}
 	}
 
 	private void setSelected(final Participation participation, final boolean selected) {
-		final CheckBox button = selectParticipationButtons.get(participation);
+		final var button = selectParticipationButtons.get(participation);
 		if (button != null && button.isSelected() != selected) {
 			button.setSelected(selected);
 		}
@@ -133,7 +147,7 @@ public class AppController {
 	}
 
 	private boolean toggleSelected(final Participation participation) {
-		final boolean selected = isSelected(participation);
+		final var selected = isSelected(participation);
 		setSelected(participation, ! selected);
 		return ! selected;
 	}
@@ -141,9 +155,9 @@ public class AppController {
 	private final Map<Action<Participation>, Button> participationActions = new HashMap<>();
 
 	private void handleParticipationActionButtonsEnablement() {
-		for (final Action<Participation> action : participationActions.keySet()) {
-			boolean disabled = selection.isEmpty();
-			for (final Participation participation : selection) {
+		for (final var action : participationActions.keySet()) {
+			var disabled = selection.isEmpty();
+			for (final var participation : selection) {
 				if (! action.isFor(participation)) {
 					disabled = true;
 					break;
@@ -154,34 +168,41 @@ public class AppController {
 	}
 
 	private List<TextField> timingKeyTextFields = null;
+	private List<TextField> participantTextFields = null;
+
+	private final Action<Participation> startParticipationAction = new Action<Participation>() {
+		@Override
+		public boolean isFor(final Participation participation) {
+			return participation.getStatus() == Participation.Status.START;
+		}
+		@Override
+		public void doFor(final Participation participation) {
+			participation.start(timeProvider.get());
+		}
+	};
 
 	private void updateGrid() {
-		final List<Node> children = competitionGrid.getChildren();
+		final var children = competitionGrid.getChildren();
 		children.clear();
+		timeCallbacks.clear();
+
 		// add headers
-		int rowIndex = 0;
-		int columnIndex = 0;
+		var rowIndex = 0;
+		var columnIndex = 0;
 
-		addNode(() -> selectAllButton, rowIndex, columnIndex++, children);
-		addNode(() -> participantLabel, rowIndex, columnIndex++, children);
-		registerAction(addNode(() -> startButton, rowIndex, columnIndex++, children), new Action<Participation>() {
-			@Override
-			public boolean isFor(final Participation participation) {
-				return participation.getStatus() == Participation.Status.START;
-			}
-			@Override
-			public void doFor(final Participation participation) {
-				participation.start(timeProvider.get());
-			}
-		});
+		addNode(selectAllButton, rowIndex, columnIndex++, children);
+		addNode(participantLabel, rowIndex, columnIndex++, children);
+		registerAction(addNode(startButton, rowIndex, columnIndex++, children), startParticipationAction);
 
-		final List<String> timingKeys = competition.getTimingKeys();
-		for (final String timingKey : timingKeys) {
+		final var timingKeys = competition.getTimingKeys();
+		for (final var timingKey : timingKeys) {
 			if (timingKeyTextFields != null) {
-				final TextField textField = addNode(() -> new TextField(timingKey), rowIndex, columnIndex++, children);
+				final var textField = addClearableTextField(timingKey, rowIndex, columnIndex, children);
 				timingKeyTextFields.add(textField);
 			} else {
-				final Button timingKeyButton = addNode(() ->  new Button(timingKey), rowIndex, columnIndex++, children);
+				final var timingKeyButton = addNode(new Button(), rowIndex, columnIndex++, children);
+				copyLookNFeel(timeButton, timingKeyButton);
+				timingKeyButton.setText(timingKey);
 				registerAction(timingKeyButton, new Action<Participation>() {
 					@Override
 					public boolean isFor(final Participation participation) {
@@ -194,53 +215,84 @@ public class AppController {
 				});
 			}
 		}
-		addNode(() -> editTimingKeysButton, rowIndex, columnIndex++, children)
-
+		addNode(editTimingKeysButton, rowIndex, columnIndex++, children)
 		.setSelected(timingKeyTextFields != null);
-		addNode(() -> addTimingKeyButton, rowIndex, columnIndex++, children);
+		addNode(addTimingKeyButton, rowIndex, columnIndex++, children);
 		rowIndex++;
 		columnIndex = 0;
-		final Iterator<Participation> participations = competition.participations();
+		final var participations = competition.participations();
 		while (participations.hasNext()) {
-			final Participation participation = participations.next();
+			final var participation = participations.next();
 
-			final CheckBox selectButton = addNode(() -> new CheckBox(), rowIndex, columnIndex++, children);
+			final var selectButton = addNode(new CheckBox(), rowIndex, columnIndex++, children);
 			selectButton.setOnAction(event -> {
 				setSelected(participation, selectButton.isSelected());
 				selectAllButton.setSelected(false);
 			});
 			selectParticipationButtons.put(participation, selectButton);
 
-			addNode(() -> new Label(participation.getParticipant().getName()), rowIndex, columnIndex++, children)
-			.setOnMouseClicked(event -> toggleSelected(participation));
+			final String participantName = participation.getParticipant().getName();
+			if (participantTextFields != null) {
+				final var textField = addClearableTextField(participantName, rowIndex, columnIndex++, children);
+				participantTextFields.add(textField);
+			} else {
+				addNode(new Label(participantName), rowIndex, columnIndex++, children);
+			}
 
-			final LocalTime startTime = participation.getStartTime();
-			addNode(() -> new Label(formatTime(startTime)), rowIndex, columnIndex++, children);
-
-			Duration last = Duration.ZERO;
-			final Iterator<String> timingKeysIt = timingKeys.iterator();
+			if (participation.getStartTime().isPresent()) {
+				final var durationLabel = addNode(new Label(formatTime((Duration) null)), rowIndex, columnIndex++, children);
+				timeCallbacks.add(time -> {
+					final var startTime = participation.getStartTime();
+					final var durationString = formatTime(Duration.between(startTime.get(), participation.getCompetition().getCurrentTime()));
+					durationLabel.setText(durationString);
+					return null;
+				});
+			} else {
+				final var startParticipationButton = addNode(new Button(), rowIndex, columnIndex++, children);
+				copyLookNFeel(startButton, startParticipationButton);
+				startParticipationButton.setOnAction(event -> {
+					if (startParticipationAction.isFor(participation)) {
+						startParticipationAction.doFor(participation);
+						updateGrid();
+					}
+				});
+			}
+			var last = Duration.ZERO;
+			final var timingKeysIt = timingKeys.iterator();
 			while (timingKeysIt.hasNext()) {
-				final String timingKey = timingKeysIt.next();
-				final Optional<Duration> durationOpt = participation.getDuration(timingKey);
-				String durationString = "__:__";
+				final var timingKey = timingKeysIt.next();
+				final var durationOpt = participation.getDuration(timingKey);
 				if (durationOpt.isPresent()) {
-					final Duration duration = durationOpt.get();
-					final Duration labelDuration = (timingKeysIt.hasNext() ? duration.minus(last) : duration);
-					durationString = formatTime(labelDuration);
+					final var duration = durationOpt.get();
+					final var labelDuration = (timingKeysIt.hasNext() ? duration.minus(last) : duration);
 					last = duration;
+					addNode(new Label(formatTime(labelDuration)), rowIndex, columnIndex++, children);
+				} else {
+					final var timeParticipationButton = addNode(new Button(), rowIndex, columnIndex++, children);
+					copyLookNFeel(timeButton, timeParticipationButton);
+					timeParticipationButton.setText("");
+					timeParticipationButton.setOnAction(event -> {
+						if (participation.getStatus() == Participation.Status.ACTIVE && (! participation.hasTime(timingKey))) {
+							participation.time(timeProvider.get(), timingKey);
+							updateGrid();
+						}
+					});
 				}
-				final String label = durationString;
-				addNode(() -> new Label(label), rowIndex, columnIndex++, children);
 			}
 			rowIndex++;
 			columnIndex = 0;
 		}
+		addNode(editParticipantNamesButton, rowIndex++, 1, children)
+		.setSelected(participantTextFields != null);
+		addNode(addParticipantButton, rowIndex++, 1, children);
+		rowIndex++;
+		columnIndex = 0;
 	}
 
 	@FXML
-	public void handleEditTimingKeys() {
+	private void handleEditTimingKeys() {
 		if (timingKeyTextFields != null) {
-			final List<String> newTimingKeys = getEditedTimingKeys(true);
+			final var newTimingKeys = getEditedText(timingKeyTextFields, true);
 			competition.setTimingKeys(newTimingKeys.toArray(new String[newTimingKeys.size()]));
 			timingKeyTextFields = null;
 		} else {
@@ -249,27 +301,62 @@ public class AppController {
 		updateGrid();
 	}
 
-	private List<String> getEditedTimingKeys(final boolean filterEmpty) {
-		return timingKeyTextFields.stream()
+	private List<String> getEditedText(final Collection<TextField> textFields, final boolean filterEmpty) {
+		return textFields.stream()
 				.map(TextField::getText)
 				.filter(text -> (! filterEmpty) || text.trim().length() > 0)
 				.collect(Collectors.toList());
 	}
 
 	@FXML
-	public void handleAddTimingKey() {
-		final List<String> newTimingKeys = (timingKeyTextFields != null ? getEditedTimingKeys(false) :	new ArrayList<>(competition.getTimingKeys()));
+	private void handleAddTimingKey() {
+		final var newTimingKeys = (timingKeyTextFields != null ? getEditedText(timingKeyTextFields, false) :	new ArrayList<>(competition.getTimingKeys()));
 		newTimingKeys.add("");
 		competition.setTimingKeys(newTimingKeys.toArray(new String[newTimingKeys.size()]));
 		timingKeyTextFields = new ArrayList<>();
 		updateGrid();
 	}
 
+	@FXML
+	private void handleEditParticipantNames() {
+		if (participantTextFields != null) {
+			updateEditedParticipants();
+			participantTextFields = null;
+		} else {
+			participantTextFields = new ArrayList<>();
+		}
+		updateGrid();
+	}
+
+	private void updateEditedParticipants() {
+		if (participantTextFields != null) {
+			final var newParticipantNames = getEditedText(participantTextFields, false);
+			int participantNum = 0;
+			for (final String name : newParticipantNames) {
+				final var participant = competition.getParticipation(participantNum).getParticipant();
+				if (name.isBlank()) {
+					competition.removeParticipant(participant);
+				} else {
+					participant.setName(name);
+					participantNum++;
+				}
+			}
+		}
+	}
+
+	@FXML
+	private void handleAddParticipant() {
+		updateEditedParticipants();
+		competition.addParticipant(new Participant(""));
+		participantTextFields = new ArrayList<>();
+		updateGrid();
+	}
+
 	private void registerAction(final Button button, final Action<Participation> action) {
 		participationActions.put(action, button);
 		button.setOnAction(event -> {
-			int count = 0;
-			for (final Participation participation : selection) {
+			var count = 0;
+			for (final var participation : selection) {
 				if (action.isFor(participation)) {
 					action.doFor(participation);
 					count++;
@@ -282,11 +369,36 @@ public class AppController {
 		});
 	}
 
-	private <T extends Node> T addNode(final Supplier<T> supplier, final int rowIndex, final int columnIndex, final Collection<Node> children) {
-		final T node = supplier.get();
+	@FXML
+	private Button clearTextFieldButton;
+
+	private TextField addClearableTextField(final String text, final int rowIndex, final int columnIndex, final Collection<Node> children) {
+		final var textField = new TextField(text);
+		final var clearButton = new Button();
+		copyLookNFeel(clearTextFieldButton, clearButton);
+		clearButton.setOnAction(event -> textField.clear());
+		addNode(new HBox(textField, clearButton), rowIndex, columnIndex, children);
+		return textField;
+	}
+
+	private <T extends Node> T addNode(final T node, final int rowIndex, final int columnIndex, final Collection<Node> children) {
+		System.out.println("Adding " + node + " @ " + rowIndex + ", " + columnIndex);
 		GridPane.setConstraints(node, columnIndex, rowIndex);
 		children.add(node);
 		return node;
+	}
+
+	private void copyLookNFeel(final Node source, final Node target) {
+		target.setStyle(source.getStyle());
+		target.getStyleClass().addAll(source.getStyleClass());
+	}
+
+	private void copyLookNFeel(final Button source, final Button target) {
+		copyLookNFeel((Node) source, (Node) target);
+		target.setText(source.getText());
+		if (source.getGraphic() instanceof ImageView) {
+			target.setGraphic(new ImageView(((ImageView) source.getGraphic()).getImage()));
+		}
 	}
 
 	@FXML
